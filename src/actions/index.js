283149fec12aa94d212405f24df1c0cc0cfbc32c
Asim Ashraf <asimgalaxy@yahoo.com>
2018-06-05 14:46:30 +0300

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'fe3dcb7002c7322e4fa4a5475eebdcba';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
